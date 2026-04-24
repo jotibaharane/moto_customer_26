38d39897f6487ce31bd90d9b71d65d69ca374d4b
jotibaharane1 <jotiba@motohelpindia.com>
2026-04-24 16:56:56 +0530

    apk size reduce

--- android/app/.cxx/Debug/6v536gy4/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
-set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Intel/Desktop/moto_customer_app/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/3k32h759/obj/armeabi-v7a/libreanimated.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Intel/Desktop/moto_customer_app/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-

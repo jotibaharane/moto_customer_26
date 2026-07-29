4506b74a2b58c012782b2cacb691466a2702057b
jotiba harane <jotibaharane1906@gmail.com>
2026-07-29 17:06:18 +0530

    working latest code

---
+++ android/app/.cxx/Debug/4p197029/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-reanimated::reanimated)
+add_library(react-native-reanimated::reanimated SHARED IMPORTED)
+set_target_properties(react-native-reanimated::reanimated PROPERTIES
+    IMPORTED_LOCATION "/home/motohelp2/Desktop/jotiba/moto_customer_26/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/376p4z11/obj/armeabi-v7a/libreanimated.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/home/motohelp2/Desktop/jotiba/moto_customer_26/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+

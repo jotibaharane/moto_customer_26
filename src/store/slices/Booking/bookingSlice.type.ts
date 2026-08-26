f0e54bf84824793b0a11bd21223ae7ee23177ad7
jotibaharane <jotibaharane1906@gmail.com>
2026-08-26 12:00:31 +0530

    working change

--- src/store/slices/Booking/bookingSlice.type.ts
+++ src/store/slices/Booking/bookingSlice.type.ts
@@ -1,66 +1,75 @@
-import { CreateLoadRequest } from '@api/type';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-interface BookingState extends CreateLoadRequest {
-  selectedDriverId: string;
-
-  driverName: string;
-  driverMobile: string;
-
-  vehicleType: string;
-  vehicleNumber: string;
-  vehicleImage: string;
-
-  weightRange: string;
-
-  freightAmount: number;
-  expectedVehicleAvailability: string;
-
-  distance: number;
-}
-
-const initialState: BookingState = {
-  customerId: '',
-
-  selectedDriverId: '',
-
-  driverName: '',
-  driverMobile: '',
-
-  vehicleType: '',
-  vehicleNumber: '',
-  vehicleImage: '',
-
-  weightRange: '',
-
-  freightAmount: 0,
-  expectedVehicleAvailability: '',
-
-  distance: 0,
-
-  pickup: {
-    name: '',
-    fullAddress: '',
-    latitude: undefined,
-    longitude: undefined,
-    plotBuilding: '',
-    streetArea: '',
-    contactName: '',
-    contactMobile: '',
-    tag: '',
-  },
-
-  delivery: {
-    name: '',
-    fullAddress: '',
-    latitude: undefined,
-    longitude: undefined,
-    plotBuilding: '',
-    streetArea: '',
-    contactName: '',
-    contactMobile: '',
-    tag: '',
-  },
-
-  weight: 0,
+export type Location = {
+  latitude: number;
+  longitude: number;
 };
+
+export type Vehicle = {
+  vehicleType: string;
+  approximateWeightKg: string;
+  vehicleNo: string;
+  weightRange:string;
+  vehicleImage:string;
+  width:string;
+  height:string;
+  length:string
+};
+
+export type Load = {
+  freight_amount: string;
+  paymentMode: string;
+  expectedVehicleAvailability: string;
+};
+
+export type BookingDetails = {
+  customerId: string;
+  LoadPost_ID?: string;
+  pickup: any;
+  delivery: any;
+  vehicle: Vehicle;
+  load: Load;
+};
+
+export type IsConfirmed = {
+  DriverID: string;
+  loadId: string;
+  loadstatus: 'waiting' | 'accepted' | 'rejected';
+};
+export type BookingState = {
+  current_location: Location;
+  booking: BookingDetails;
+  pickupLocation: Location;
+  dropLocation: Location;
+  isConfirmed?: IsConfirmed;
+  bookingVehicle?: any;
+  avilableVehicles?: VehicleLocation[];
+  DriverID?: any;
+};
+
+export interface VehicleLocation {
+  vehicleid: string;
+  registration_no: string;
+  vehicleType: string;
+  body_type: string;
+  VehicleName: string;
+  WeightRange: string;
+
+  Lat: number;
+  Lng: number;
+
+  DriverID: string;
+  MobileNo: string;
+
+  VehicleID: string;
+  VendorID: string;
+
+  freight_amount: string;
+
+  paymentMode: string | null;
+  expectedVehicleAvailability: string | null;
+
+  LP_Status: string;
+
+  Img: string;
+
+  DistanceKm: number;
+}

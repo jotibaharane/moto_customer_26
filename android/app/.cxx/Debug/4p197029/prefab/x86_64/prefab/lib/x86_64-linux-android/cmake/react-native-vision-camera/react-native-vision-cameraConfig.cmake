972e6f21f49d031c0e38edae7e98c8146ecdc909
jotiba harane <jotibaharane1906@gmail.com>
2026-09-11 13:11:20 +0530

    working

---
+++ android/app/.cxx/Debug/4p197029/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/react-native-vision-camera/react-native-vision-cameraConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-vision-camera::VisionCamera)
+add_library(react-native-vision-camera::VisionCamera SHARED IMPORTED)
+set_target_properties(react-native-vision-camera::VisionCamera PROPERTIES
+    IMPORTED_LOCATION "/home/motohelp2/Desktop/jotiba/moto_customer_26/node_modules/react-native-vision-camera/android/build/intermediates/cxx/Debug/1q6i4d4a/obj/x86_64/libVisionCamera.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/home/motohelp2/Desktop/jotiba/moto_customer_26/node_modules/react-native-vision-camera/android/build/headers/visioncamera"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
